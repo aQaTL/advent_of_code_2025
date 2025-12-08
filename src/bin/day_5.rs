5f852f228d5dd3aefbe9a30c56b09b1f0333636e
aqatl <mmsoltys@outlook.com>
2025-12-08 01:33:43 +0100

    Use tabs

--- src/bin/day_5.rs
+++ src/bin/day_5.rs
@@ -1,77 +1,77 @@
 use itertools::Itertools;
 
 fn main() {
-    let input = std::fs::read_to_string("input/day_5.txt").unwrap();
-    println!("Part 1: {}", p1(&input));
-    println!("Part 2: {}", p2(&input));
+	let input = std::fs::read_to_string("input/day_5.txt").unwrap();
+	println!("Part 1: {}", p1(&input));
+	println!("Part 2: {}", p2(&input));
 }
 
 fn p1(input: &str) -> usize {
-    let (ranges, ids) = input.split("\n\n").collect_tuple().unwrap();
-    let ranges: Vec<(i64, i64)> = ranges
-        .lines()
-        .map(|l| {
-            l.split('-')
-                .map(|n| n.parse().unwrap())
-                .collect_tuple()
-                .unwrap()
-        })
-        .collect();
-    let ids: Vec<i64> = ids.lines().map(|n| n.parse().unwrap()).collect();
+	let (ranges, ids) = input.split("\n\n").collect_tuple().unwrap();
+	let ranges: Vec<(i64, i64)> = ranges
+		.lines()
+		.map(|l| {
+			l.split('-')
+				.map(|n| n.parse().unwrap())
+				.collect_tuple()
+				.unwrap()
+		})
+		.collect();
+	let ids: Vec<i64> = ids.lines().map(|n| n.parse().unwrap()).collect();
 
-    ids.into_iter()
-        .filter(|id| {
-            ranges
-                .iter()
-                .any(|(start, end)| (start..=end).contains(&id))
-        })
-        .count()
+	ids.into_iter()
+		.filter(|id| {
+			ranges
+				.iter()
+				.any(|(start, end)| (start..=end).contains(&id))
+		})
+		.count()
 }
 
 fn p2(input: &str) -> i64 {
-    let (ranges, _) = input.split("\n\n").collect_tuple().unwrap();
-    let mut ranges: Vec<(i64, i64)> = ranges
-        .lines()
-        .map(|l| {
-            l.split('-')
-                .map(|n| n.parse().unwrap())
-                .collect_tuple()
-                .unwrap()
-        })
-        .collect();
+	let (ranges, _) = input.split("\n\n").collect_tuple().unwrap();
+	let mut ranges: Vec<(i64, i64)> = ranges
+		.lines()
+		.map(|l| {
+			l.split('-')
+				.map(|n| n.parse().unwrap())
+				.collect_tuple()
+				.unwrap()
+		})
+		.collect();
 
-    loop {
-        let mut consolidated_ranges = Vec::<(i64, i64)>::new();
-        consolidated_ranges.push(ranges[0].clone());
+	loop {
+		let mut consolidated_ranges = Vec::<(i64, i64)>::new();
+		consolidated_ranges.push(ranges[0].clone());
 
-        let mut changed_anything = false;
-        'l: for range in ranges.into_iter().skip(1) {
-            for con_range in consolidated_ranges.iter_mut() {
-                if overlaps(range, *con_range) {
-                    changed_anything = true;
-                    *con_range = (range.0.min(con_range.0), con_range.1.max(range.1));
-                    continue 'l;
-                }
-            }
-            consolidated_ranges.push(range);
-        }
-        ranges = consolidated_ranges;
+		let mut changed_anything = false;
+		'l: for range in ranges.into_iter().skip(1) {
+			for con_range in consolidated_ranges.iter_mut() {
+				if overlaps(range, *con_range) {
+					changed_anything = true;
+					*con_range = (range.0.min(con_range.0), con_range.1.max(range.1));
+					continue 'l;
+				}
+			}
+			consolidated_ranges.push(range);
+		}
+		ranges = consolidated_ranges;
 
-        if !changed_anything {
-            break;
-        }
-    }
+		if !changed_anything {
+			break;
+		}
+	}
 
-    ranges.into_iter().map(|(start, end)| end - start + 1).sum()
+	ranges.into_iter().map(|(start, end)| end - start + 1).sum()
 }
 
 fn overlaps(a: (i64, i64), b: (i64, i64)) -> bool {
-    a.0 <= b.1 && a.0 >= b.0 || b.0 <= a.1 && b.0 >= a.0
+	a.0 <= b.1 && a.0 >= b.0 || b.0 <= a.1 && b.0 >= a.0
 }
 
 #[cfg(test)]
 mod tests {
-    const INPUT: &str = "\
+	const INPUT: &str = "\
 3-5
 10-14
 16-20
@@ -85,21 +85,21 @@ mod tests {
 32\
     ";
 
-    #[test]
-    fn p1() {
-        let expected = 3;
-        assert_eq!(super::p1(INPUT), expected);
-    }
+	#[test]
+	fn p1() {
+		let expected = 3;
+		assert_eq!(super::p1(INPUT), expected);
+	}
 
-    #[test]
-    fn p2() {
-        let expected = 14;
-        assert_eq!(super::p2(INPUT), expected);
-    }
+	#[test]
+	fn p2() {
+		let expected = 14;
+		assert_eq!(super::p2(INPUT), expected);
+	}
 
-    #[test]
-    fn p2_test_1() {
-        const INPUT: &str = "\
+	#[test]
+	fn p2_test_1() {
+		const INPUT: &str = "\
 3-5
 10-14
 16-20
@@ -114,13 +114,13 @@ mod tests {
 17
 32\
         ";
-        let expected = 14;
-        assert_eq!(super::p2(INPUT), expected);
-    }
+		let expected = 14;
+		assert_eq!(super::p2(INPUT), expected);
+	}
 
-    #[test]
-    fn p2_test_2() {
-        const INPUT: &str = "\
+	#[test]
+	fn p2_test_2() {
+		const INPUT: &str = "\
 1-10
 15-20
 17-25
@@ -128,38 +128,38 @@ mod tests {
 1
 2\
         ";
-        let expected = 21;
-        assert_eq!(super::p2(INPUT), expected);
-    }
+		let expected = 21;
+		assert_eq!(super::p2(INPUT), expected);
+	}
 
-    #[test]
-    fn p2_test_3() {
-        const INPUT: &str = "\
+	#[test]
+	fn p2_test_3() {
+		const INPUT: &str = "\
 1-10
 1-10
 
 1
 2\
         ";
-        let expected = 10;
-        assert_eq!(super::p2(INPUT), expected);
-    }
-    #[test]
-    fn p2_test_4() {
-        const INPUT: &str = "\
+		let expected = 10;
+		assert_eq!(super::p2(INPUT), expected);
+	}
+	#[test]
+	fn p2_test_4() {
+		const INPUT: &str = "\
 1-10
 2-5
 
 1
 2\
         ";
-        let expected = 10;
-        assert_eq!(super::p2(INPUT), expected);
-    }
+		let expected = 10;
+		assert_eq!(super::p2(INPUT), expected);
+	}
 
-    #[test]
-    fn p2_test_5() {
-        const INPUT: &str = "\
+	#[test]
+	fn p2_test_5() {
+		const INPUT: &str = "\
 200-300
 100-101
 1-1
@@ -189,7 +189,7 @@ mod tests {
 
 2\
         ";
-        let expected = 202;
-        assert_eq!(super::p2(INPUT), expected);
-    }
+		let expected = 202;
+		assert_eq!(super::p2(INPUT), expected);
+	}
 }
--- src/bin/day_7.rs
+++ src/bin/day_7.rs
@@ -3,108 +3,108 @@ use std::collections::{HashSet, VecDeque};
 use itertools::Itertools;
 
 fn main() {
-    let input = std::fs::read_to_string("input/day_7.txt").unwrap();
-    println!("Part 1: {}", p1(&input));
-    println!("Part 2: {}", p2(&input));
+	let input = std::fs::read_to_string("input/day_7.txt").unwrap();
+	println!("Part 1: {}", p1(&input));
+	println!("Part 2: {}", p2(&input));
 }
 
 fn p1(input: &str) -> i64 {
-    let grid: Vec<Vec<u8>> = input.lines().map(|l| l.as_bytes().to_vec()).collect_vec();
-    let start = (0_usize, grid[0].iter().position(|c| *c == b'S').unwrap());
+	let grid: Vec<Vec<u8>> = input.lines().map(|l| l.as_bytes().to_vec()).collect_vec();
+	let start = (0_usize, grid[0].iter().position(|c| *c == b'S').unwrap());
 
-    let mut streams = VecDeque::<(usize, usize)>::new();
-    streams.push_back(start);
-    let mut visited = HashSet::new();
-    let mut splits = 0;
+	let mut streams = VecDeque::<(usize, usize)>::new();
+	streams.push_back(start);
+	let mut visited = HashSet::new();
+	let mut splits = 0;
 
-    while let Some((mut stream_j, stream_i)) = streams.pop_front() {
-        stream_j += 1;
-        visited.insert((stream_j, stream_i));
-        match grid.get(stream_j).and_then(|row| row.get(stream_i)) {
-            Some(b'.') => {
-                streams.push_back((stream_j, stream_i));
-                continue;
-            }
-            Some(b'^') => {
-                let new_stream_1 = (stream_j, stream_i - 1);
-                let new_stream_2 = (stream_j, stream_i + 1);
-                let visited_new_stream_1 = visited.contains(&new_stream_1);
-                let visited_new_stream_2 = visited.contains(&new_stream_2);
-                if !visited_new_stream_1 {
-                    streams.push_back(new_stream_1);
-                    visited.insert(new_stream_1);
-                }
-                if !visited_new_stream_2 {
-                    streams.push_back(new_stream_2);
-                    visited.insert(new_stream_2);
-                }
-                if !visited_new_stream_1 || !visited_new_stream_2 {
-                    splits += 1;
-                }
-            }
-            None => {
-                continue;
-            }
+	while let Some((mut stream_j, stream_i)) = streams.pop_front() {
+		stream_j += 1;
+		visited.insert((stream_j, stream_i));
+		match grid.get(stream_j).and_then(|row| row.get(stream_i)) {
+			Some(b'.') => {
+				streams.push_back((stream_j, stream_i));
+				continue;
+			}
+			Some(b'^') => {
+				let new_stream_1 = (stream_j, stream_i - 1);
+				let new_stream_2 = (stream_j, stream_i + 1);
+				let visited_new_stream_1 = visited.contains(&new_stream_1);
+				let visited_new_stream_2 = visited.contains(&new_stream_2);
+				if !visited_new_stream_1 {
+					streams.push_back(new_stream_1);
+					visited.insert(new_stream_1);
+				}
+				if !visited_new_stream_2 {
+					streams.push_back(new_stream_2);
+					visited.insert(new_stream_2);
+				}
+				if !visited_new_stream_1 || !visited_new_stream_2 {
+					splits += 1;
+				}
+			}
+			None => {
+				continue;
+			}
 
-            _ => panic!(),
-        }
-    }
+			_ => panic!(),
+		}
+	}
 
-    splits
+	splits
 }
 
 fn p2(input: &str) -> i64 {
-    let grid: Vec<Vec<u8>> = input.lines().map(|l| l.as_bytes().to_vec()).collect_vec();
-    let start = (0_usize, grid[0].iter().position(|c| *c == b'S').unwrap(), 1);
+	let grid: Vec<Vec<u8>> = input.lines().map(|l| l.as_bytes().to_vec()).collect_vec();
+	let start = (0_usize, grid[0].iter().position(|c| *c == b'S').unwrap(), 1);
 
-    let mut streams = VecDeque::<(usize, usize, i64)>::new();
-    streams.push_back(start);
-    let mut realities = 0;
+	let mut streams = VecDeque::<(usize, usize, i64)>::new();
+	streams.push_back(start);
+	let mut realities = 0;
 
-    while let Some((mut stream_j, stream_i, particle_count)) = streams.pop_front() {
-        stream_j += 1;
+	while let Some((mut stream_j, stream_i, particle_count)) = streams.pop_front() {
+		stream_j += 1;
 
-        match grid.get(stream_j).and_then(|row| row.get(stream_i)) {
-            Some(b'.') => {
-                streams.push_back((stream_j, stream_i, particle_count));
-                continue;
-            }
-            Some(b'^') => {
-                let new_stream_1_i = stream_i - 1;
-                let new_stream_2_i = stream_i + 1;
+		match grid.get(stream_j).and_then(|row| row.get(stream_i)) {
+			Some(b'.') => {
+				streams.push_back((stream_j, stream_i, particle_count));
+				continue;
+			}
+			Some(b'^') => {
+				let new_stream_1_i = stream_i - 1;
+				let new_stream_2_i = stream_i + 1;
 
-                if let Some((_, _, pc)) = streams
-                    .iter_mut()
-                    .find(|(j, i, _)| (*j, *i) == (stream_j, new_stream_1_i))
-                {
-                    *pc += particle_count;
-                } else {
-                    streams.push_back((stream_j, new_stream_1_i, particle_count));
-                }
+				if let Some((_, _, pc)) = streams
+					.iter_mut()
+					.find(|(j, i, _)| (*j, *i) == (stream_j, new_stream_1_i))
+				{
+					*pc += particle_count;
+				} else {
+					streams.push_back((stream_j, new_stream_1_i, particle_count));
+				}
 
-                if let Some((_, _, pc)) = streams
-                    .iter_mut()
-                    .find(|(j, i, _)| (*j, *i) == (stream_j, new_stream_2_i))
-                {
-                    *pc += particle_count;
-                } else {
-                    streams.push_back((stream_j, new_stream_2_i, particle_count));
-                }
-            }
-            None => {
-                realities += particle_count;
-                continue;
-            }
-            _ => panic!(),
-        }
-    }
+				if let Some((_, _, pc)) = streams
+					.iter_mut()
+					.find(|(j, i, _)| (*j, *i) == (stream_j, new_stream_2_i))
+				{
+					*pc += particle_count;
+				} else {
+					streams.push_back((stream_j, new_stream_2_i, particle_count));
+				}
+			}
+			None => {
+				realities += particle_count;
+				continue;
+			}
+			_ => panic!(),
+		}
+	}
 
-    realities
+	realities
 }
 
 #[cfg(test)]
 mod tests {
-    const INPUT: &str = "\
+	const INPUT: &str = "\
 .......S.......
 ...............
 .......^.......
@@ -123,15 +123,15 @@ mod tests {
 ...............\
     ";
 
-    #[test]
-    fn p1() {
-        let expected = 21;
-        assert_eq!(super::p1(INPUT), expected);
-    }
+	#[test]
+	fn p1() {
+		let expected = 21;
+		assert_eq!(super::p1(INPUT), expected);
+	}
 
-    #[test]
-    fn p2() {
-        let expected = 40;
-        assert_eq!(super::p2(INPUT), expected);
-    }
+	#[test]
+	fn p2() {
+		let expected = 40;
+		assert_eq!(super::p2(INPUT), expected);
+	}
 }
